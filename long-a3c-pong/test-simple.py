ec2af144aeddd000451dcf633857588cc0361b5c
daniyalusmani <daniyal.usmani@dreambroker.fi>
2018-12-09 00:45:43 +0200

    long play pong config

---
+++ long-a3c-pong/test-simple.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  7 10:57:21 2018
+
+@author: daniyalusmani
+"""
+import time
+from collections import deque
+import argparse
+import torch
+import torch.nn.functional as F
+
+from envs import ObsNorm
+from model import ActorCritic
+from pong import Pong
+from simple_ai import PongAi
+import matplotlib.pyplot as plt
+
+
+parser = argparse.ArgumentParser(description='A3C')
+parser.add_argument("--load_checkpoint", default=False, help="Load the checkpoint and run from there")
+
+def load_checkpoint(model, filename='/output/checkpoint.pth.tar'):
+    checkpoint = torch.load(filename)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+
+def train(args):
+    env = Pong(headless= False)
+    
+    model = ActorCritic(1, 3)
+    
+    opponent = PongAi(env, 2)
+    
+    model.eval()
+    obsNorm = ObsNorm();
+    
+    env.set_names('Player', opponent.get_name())
+    state = obsNorm.prepro(env.reset()[0])
+    
+    state = torch.from_numpy(state)
+    reward_sum = 0
+    done = True
+    
+    done_count = 0
+    actions = []
+
+    if args.load_checkpoint:
+        load_checkpoint(model, args.load_checkpoint)
+    
+    while True:
+        env.render()
+        # Sync with the shared model
+        if done:
+            cx = torch.zeros(1, 256)
+            hx = torch.zeros(1, 256)
+        else:
+            cx = cx.detach()
+            hx = hx.detach()
+    
+        with torch.no_grad():
+            inputTensor = state.unsqueeze(0);
+            value, logit, (hx, cx) = model((inputTensor, (hx, cx)))
+        prob = F.softmax(logit, dim=-1)
+        print(prob)
+        action = prob.max(1, keepdim=True)[1].numpy()
+    
+        action2 = opponent.get_action()
+        
+        (state, obs2), (reward, reward2), done, info = env.step((action[0,0], action2))
+        
+        
+        done = done or done_count >= 500
+        reward_sum += reward
+        state = obsNorm.prepro(state)
+        
+        #print(state.squeeze(0).shape)
+        #plt.imshow(state.squeeze(0))
+        #plt.show()
+        
+        actions.append(action[0, 0])
+        
+        if actions.count(actions[0]) == 5000:
+            done = True
+        if done:
+           actions.clear()
+           state = obsNorm.prepro(env.reset()[0])
+           obsNorm.reset()
+           done_count +=1
+        if done_count == 20:
+            break;
+        
+        state = torch.from_numpy(state)
+            
+if __name__ == "__main__":           
+    args = parser.parse_args()
+    train(args)
